d6a7a8c4e9663a195d4192b13ffbe381c980691f
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-06-09 11:22:42 +0100

    Remove redundant shadow mixin variable

--- src/main/java/io/github/thatsmusic99/norefreshscroll/mixin/MultiplayerScreenMixin.java
+++ src/main/java/io/github/thatsmusic99/norefreshscroll/mixin/MultiplayerScreenMixin.java
@@ -5,14 +5,11 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MultiplayerScreen.class)
 public class MultiplayerScreenMixin {
-
-    @Shadow private boolean initialized;
 
     @Redirect(method = "refresh", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
     public void realignScrollBar(MinecraftClient client, Screen screen) {
