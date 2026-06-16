b5fc5decd82247fcb69713f77ced210e8c49e14d
Thatsmusic99 <25277367+thatsmusic99@users.noreply.github.com>
2026-06-16 20:44:15 +0100

    chore(mc): update to 26.2

--- src/main/java/io/github/thatsmusic99/norefreshscroll/mixin/JoinMultiplayerScreenMixin.java
+++ src/main/java/io/github/thatsmusic99/norefreshscroll/mixin/JoinMultiplayerScreenMixin.java
@@ -1,6 +1,6 @@
 package io.github.thatsmusic99.norefreshscroll.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
@@ -11,15 +11,15 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(JoinMultiplayerScreen.class)
 public class JoinMultiplayerScreenMixin {
 
-    @Redirect(method = "refreshServerList", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"))
-    public void realignScrollBar(Minecraft client, Screen screen) {
+    @Redirect(method = "refreshServerList", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"))
+    public void realignScrollBar(Gui gui, Screen screen) {
         if (!(screen instanceof JoinMultiplayerScreen)) {
-            client.setScreen(screen);
+            gui.setScreen(screen);
             return;
         }
-        Screen currentScreen = client.screen;
+        Screen currentScreen = gui.screen();
         if (!(currentScreen instanceof JoinMultiplayerScreen multiplayerScreen)) {
-            client.setScreen(screen);
+            gui.setScreen(screen);
             return;
         }
         ServerSelectionList oldListWidget = ((JoinMultiplayerScreenAccessor) multiplayerScreen).getServerSelectionList();
@@ -27,7 +27,7 @@ public class JoinMultiplayerScreenMixin {
         double scrollAmount = oldListWidget.scrollAmount();
 
         // Set the screen.
-        client.setScreen(screen);
+        gui.setScreen(screen);
 
         // Get the new server list widget.
         ServerSelectionList newListWidget = ((JoinMultiplayerScreenAccessor) screen).getServerSelectionList();
